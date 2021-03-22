e424a84089188c17249fe8452ca2a1c9d688cf39
cobebandiem <dung5134567890@gmail.com>
2021-03-22 14:49:58 +0700

    Ghi chú Commit

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const app = express();
 const server = app.listen(process.env.PORT || 3000, () => {
     console.log(`SERVER running`);
 });
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.get('/product', (req, res) => {
+    res.json([
+        {
+            "id":"1",
+            "name":"iphone "
+        }
+    ]);
 });
+//git push -u origin main
+//git remote add origin https://github.com/cobebandiem/testnodejs.git
